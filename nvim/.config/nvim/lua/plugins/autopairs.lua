1c6d82bfcc64d5eadfcb6ef89378d10fdf5fabde
Daniel <daniel.crockett98@gmail.com>
2025-10-16 11:46:03 +1100

    install ts-autotag to auto close html tags

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -4,4 +4,14 @@ return {
 		event = "InsertEnter",
 		config = true,
 	},
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup({
+				opts = {
+					enable_close_on_slash = true,
+				},
+			})
+		end,
+	},
 }
